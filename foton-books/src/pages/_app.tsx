2584922a6607c5c678741516ed9e1c157447fcdb
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-08-16 18:09:10 -0300

    feat: add globalstyles

--- foton-books/src/pages/_app.tsx
+++ foton-books/src/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../styles/global';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Foton Books</title>
+      </Head>
+
+      <GlobalStyles />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default MyApp;
---
+++ foton-books/src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+import * as fonts from './fonts';
+
+export default createGlobalStyle`
+  ${fonts.SFProText}
+  ${fonts.SFProDisplay}
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    :before,
+    :after {
+      box-sizing: inherit;
+    }
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+
+    img {
+      max-width: 100%;
+    }
+  `}
+`;
