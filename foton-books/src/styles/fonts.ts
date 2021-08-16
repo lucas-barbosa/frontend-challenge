500b02af2130a19e7801a4b4bdd5e34e36429ba5
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-08-16 18:07:45 -0300

    feat: add SF Pro fonts

---
+++ foton-books/src/styles/fonts.ts
@@ -0,0 +1,53 @@
+import { css } from 'styled-components';
+
+export const SFProDisplay = css`
+  @font-face {
+    font-family: 'SFProDisplay';
+    font-style: normal;
+    font-weight: 300;
+    src: local('SFProDisplay Light'), local('SFProDisplay Light'),
+      url('/fonts/SFProDisplay/SFProDisplay-Light.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SFProDisplay';
+    font-style: normal;
+    font-weight: 400;
+    src: local('SFProDisplay Regular'), local('SFProDisplay Regular'),
+      url('/fonts/SFProDisplay/SFProDisplay-Regular.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SFProDisplay';
+    font-style: normal;
+    font-weight: 600;
+    src: local('SFProDisplay Bold'), local('SFProDisplay Bold'),
+      url('/fonts/SFProDisplay/SFProDisplay-Bold.woff') format('woff');
+  }
+`;
+
+export const SFProText = css`
+  @font-face {
+    font-family: 'SFProText';
+    font-style: normal;
+    font-weight: 300;
+    src: local('SFProText Light'), local('SFProText Light'),
+      url('/fonts/SFProText/SFProText-Light.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SFProText';
+    font-style: normal;
+    font-weight: 400;
+    src: local('SFProText Regular'), local('SFProText Regular'),
+      url('/fonts/SFProText/SFProText-Regular.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'SFProText';
+    font-style: normal;
+    font-weight: 600;
+    src: local('SFProText Bold'), local('SFProText Bold'),
+      url('/fonts/SFProText/SFProText-Bold.woff') format('woff');
+  }
+`;
