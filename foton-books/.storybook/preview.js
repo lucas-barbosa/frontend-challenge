0de212252b5c286397de79b9b0c4cfdea9eadcc9
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-08-16 18:09:52 -0300

    config: make storybook works with theme file

--- foton-books/.storybook/preview.js
+++ foton-books/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../styles/global';
+import theme from '../styles/theme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,12 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
