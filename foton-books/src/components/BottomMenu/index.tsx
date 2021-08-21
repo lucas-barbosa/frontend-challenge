951d1686d23bdc6f0d7a5625c2faf7f08bfaa4d3
Lucas Barbosa <barbosa.lucash@gmail.com>
2021-08-21 11:48:07 -0300

    feat: add Links to BottomMenu

--- foton-books/src/components/BottomMenu/index.tsx
+++ foton-books/src/components/BottomMenu/index.tsx
@@ -1,27 +1,46 @@
-import * as S from './styles';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { HomeIcon, LibraryIcon, ProfileIcon } from 'components/Icons';
 import Container from 'components/Container';
 
-const BottomMenu = () => (
-  <S.Wrapper>
-    <Container as={S.Nav}>
-      <S.Link href="" role="link" active>
-        <HomeIcon title="home link" />
-        <span>Home</span>
-      </S.Link>
+import * as S from './styles';
 
-      <S.Link href="" role="link">
-        <LibraryIcon title="library link" />
-        <span>Libraries</span>
-      </S.Link>
+const menuItems = [
+  {
+    link: '/',
+    title: 'Home',
+    icon: <HomeIcon title="home link" />
+  },
+  {
+    link: '/libraries',
+    title: 'Libraries',
+    icon: <LibraryIcon title="library link" />
+  },
+  {
+    link: '/profile',
+    title: 'Profile',
+    icon: <ProfileIcon title="profile link" />
+  }
+];
 
-      <S.Link href="" role="link">
-        <ProfileIcon title="profile link" />
-        <span>Profile</span>
-      </S.Link>
-    </Container>
-  </S.Wrapper>
-);
+const BottomMenu = () => {
+  const { asPath } = useRouter();
+
+  return (
+    <S.Wrapper>
+      <Container as={S.Nav}>
+        {menuItems.map((item) => (
+          <Link key={item.link} href={item.link} passHref>
+            <S.Link role="link" active={item.link == asPath}>
+              {item.icon}
+              <span>{item.title}</span>
+            </S.Link>
+          </Link>
+        ))}
+      </Container>
+    </S.Wrapper>
+  );
+};
 
 export default BottomMenu;
